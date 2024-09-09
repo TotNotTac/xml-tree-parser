ce08dcf7a5700233344a1ac3525a45c04692f0b5
Silas de Jong <silas.de.jong@cobase.com>
2024-09-09 16:26:48 +0200

    add whitespace ignoring parser
    Cleaning up repo

--- app/Main.hs
+++ app/Main.hs
@@ -1,44 +1,42 @@
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE StrictData #-}
 
 module Main where
 
 import qualified Data.ByteString as BS
-import Data.Text (Text)
 
-import Control.Applicative (optional)
+import Data.Text (Text)
 import qualified Data.Text.IO as T
+
 import Parser
 import Xeno.DOM (Content (Element), parse)
 
 data GroupHeader = GroupHeader
-    { _msgId :: Text
-    , _creDtTm :: Text
-    , _nbOfTxs :: Int
-    , _ctrlSum :: Double
-    , _initgPty :: Text
-    }
-    deriving (Show)
+  { _msgId :: Text
+  , _creDtTm :: Text
+  , _nbOfTxs :: Int
+  , _ctrlSum :: Double
+  , _initgPty :: Text
+  }
+  deriving (Show)
 
 newtype ParsedDocument = ParsedDocument
-    { groupHeader :: GroupHeader
-    }
-    deriving (Show)
+  { groupHeader :: GroupHeader
+  }
+  deriving (Show)
 
 smallTestParser :: Parser (Text, Text)
 smallTestParser = do
-    isXmlDocument
-    withNodeDeep ["Document", "Bla"] $ do
-        message <- withNodeDeep ["Yo", "Message"] messageParser
-        binaryData <- withNode "Data" textP
-        pure (message, binaryData)
+  isXmlDocument
+  withNodeDeep ["Document", "Bla"] $ do
+    message <- withNodeDeep ["Yo", "Message"] messageParser
+    binaryData <- withNode "Data" textP
+    pure (message, binaryData)
 
 main :: IO ()
 main = do
-    Right root <- parse <$> BS.readFile "small.xml"
-    case evalParser smallTestParser [Element root] of
-        Right a -> print a
-        Left e -> T.putStrLn e
+  Right root <- parse <$> BS.readFile "small.xml"
+  case evalParser smallTestParser [Element root] of
+    Right a -> print a
+    Left e -> T.putStrLn e
--- app/Parser.hs
+++ app/Parser.hs
@@ -3,15 +3,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-module Parser where
+{-# HLINT ignore "Use tuple-section" #-}
 
-import Control.Arrow (first)
-import Data.Function ((&))
-import Data.Maybe (mapMaybe)
-import Data.Text (Text)
+module Parser (
+  ParserError,
+  Parser (..),
+  isXmlDocument,
+  withNodeDeep,
+  messageParser,
+  withNode,
+  textP,
+  evalParser,
+  execParser,
+  lexeme,
+  contentsP,
+  assertP,
+  attr,
+  satisfyContent,
+  nodeP,
+)
+where
 
 import Control.Applicative (Alternative (..))
+import Control.Arrow (first, (>>>))
+import Control.Monad (guard)
+import Data.Maybe (mapMaybe)
+import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Text.Encoding (decodeUtf8)
 import qualified Data.Text.Encoding as T
@@ -23,35 +42,35 @@ type ParserError = Text
 newtype Parser a = Parser {runParser :: [Content] -> (Either ParserError a, [Content])}
 
 instance Functor Parser where
-    fmap f p = Parser $ first (fmap f) . runParser p
+  fmap f p = Parser $ first (fmap f) . runParser p
 
 instance Applicative Parser where
-    pure :: a -> Parser a
-    pure x = Parser (Right x,)
-    (<*>) :: Parser (a -> b) -> Parser a -> Parser b
-    Parser ff <*> Parser xx = Parser $ \nodes0 -> case ff nodes0 of
-        (Left e, nodes1) -> (Left e, nodes1)
-        (Right f, nodes1) -> case xx nodes1 of
-            (Left e, nodes2) -> (Left e, nodes2)
-            (Right x, nodes2) -> (Right (f x), nodes2)
+  pure :: a -> Parser a
+  pure x = Parser (Right x,)
+  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  Parser ff <*> Parser xx = Parser $ \nodes0 -> case ff nodes0 of
+    (Left e, nodes1) -> (Left e, nodes1)
+    (Right f, nodes1) -> case xx nodes1 of
+      (Left e, nodes2) -> (Left e, nodes2)
+      (Right x, nodes2) -> (Right (f x), nodes2)
 
 instance Monad Parser where
-    (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-    (>>=) p1 p2 = Parser $ \cts -> case runParser p1 cts of
-        (Right a, _) -> runParser (p2 a) cts
-        (Left e, _) -> (Left e, cts)
+  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  (>>=) p1 p2 = Parser $ \cts -> case runParser p1 cts of
+    (Right a, _) -> runParser (p2 a) cts
+    (Left e, _) -> (Left e, cts)
 
 instance MonadFail Parser where
-    fail :: String -> Parser a
-    fail message = Parser $ \cts -> (Left $ T.pack message, cts)
+  fail :: String -> Parser a
+  fail message = Parser $ \cts -> (Left $ T.pack message, cts)
 
 instance Alternative Parser where
-    empty :: Parser a
-    empty = Parser $ const (Left "Hit an empty alternative", [])
-    (<|>) :: Parser a -> Parser a -> Parser a
-    (<|>) p1 p2 = Parser $ \cts -> case runParser p1 cts of
-        (Right x, cts') -> (Right x, cts')
-        (Left _, _) -> runParser p2 cts
+  empty :: Parser a
+  empty = Parser $ \cts -> (Left "No more parser paths", cts)
+  (<|>) :: Parser a -> Parser a -> Parser a
+  (<|>) p1 p2 = Parser $ \cts -> case runParser p1 cts of
+    (Right x, cts') -> (Right x, cts')
+    (Left _, _) -> runParser p2 cts
 
 evalParser :: Parser a -> [Content] -> Either ParserError a
 evalParser p cts = fst $ runParser p cts
@@ -59,37 +78,43 @@ evalParser p cts = fst $ runParser p cts
 execParser :: Parser a -> [Content] -> [Content]
 execParser p cts = snd $ runParser p cts
 
+rawTextP :: Parser Text
+rawTextP = Parser $ \case
+  [Text bs] -> (pure $ decodeUtf8 bs, [])
+  xs -> (Left $ fmt "Node is not a text node: " +|| xs ||+ "", xs)
+
+-- | The Lexeme function takes a parser, and transforms it into a parser that ignores the surrounding whitespace.
+lexeme
+  :: Parser Text
+  -> Parser Text
+lexeme p = T.strip <$> p
+
 textP :: Parser Text
-textP = Parser $ \case
-    [Text bs] -> (pure $ decodeUtf8 bs, [])
-    xs -> (Left $ fmt "Node is not a text node: " +|| xs ||+ "", xs)
+textP = lexeme rawTextP
 
 findNode :: Text -> [Content] -> Maybe Node
-findNode targetName cts =
-    flip
-        mapMaybe
-        cts
-        ( \case
-            (Element node) | decodeUtf8 (name node) == targetName -> Just node
-            _ -> Nothing
-        )
-        & \case
-            [n] -> Just n
-            [] -> Nothing
-            _ -> error $ fmtLn "More than one node found of type: " +|| targetName ||+ ". This should never happen"
+findNode targetName =
+  mapMaybe
+    ( \case
+        (Element node) | decodeUtf8 (name node) == targetName -> Just node
+        _ -> Nothing
+    )
+    >>> \case
+      [n] -> Just n
+      [] -> Nothing
+      _ -> error $ fmtLn "More than one node found of type: " +|| targetName ||+ ". This should never happen"
 
 withNode :: Text -> Parser a -> Parser a
 withNode targetName innerParser = Parser $
-    \cts -> case findNode targetName cts of
-        (Just innerNode) -> runParser innerParser (contents innerNode)
-        Nothing -> (Left $ fmt "Node not found: " +|| targetName ||+ "", cts)
+  \cts -> case findNode targetName cts of
+    (Just innerNode) -> runParser innerParser (contents innerNode)
+    Nothing -> (Left $ fmt "Node not found: " +|| targetName ||+ "", cts)
 
 withNodeDeep :: [Text] -> Parser a -> Parser a
-withNodeDeep (targetName : deepNames) deepParser = withNode targetName $ withNodeDeep deepNames deepParser
-withNodeDeep [] deepParser = deepParser
+withNodeDeep = flip (foldr withNode)
 
-everythingP :: Parser [Content]
-everythingP = Parser $ \cts -> (Right cts, cts)
+contentsP :: Parser [Content]
+contentsP = Parser $ \cts -> (Right cts, cts)
 
 assertP :: Bool -> Text -> Parser ()
 assertP True _ = Parser $ \cts -> (Right (), cts)
@@ -97,30 +122,30 @@ assertP False msg = fail (T.unpack msg)
 
 messageParser :: Parser Text
 messageParser = do
-    message <- textP
-    assertP ("T" `T.isPrefixOf` message) "Message doesn't follow format"
-    pure message
+  message <- textP
+  guard ("T" `T.isPrefixOf` message) <|> fail "Message doesn't follow format"
+  pure message
 
 attr :: Text -> Parser Text
 attr attrName = do
-    n <- nodeP
-    case lookup (T.encodeUtf8 attrName) (attributes n) of
-        Nothing -> fail $ fmt "Attribute not found: " +|| attrName ||+ ""
-        Just bytestringValue -> pure $ decodeUtf8 bytestringValue
+  n <- nodeP
+  case lookup (T.encodeUtf8 attrName) (attributes n) of
+    Nothing -> fail $ fmt "Attribute not found: " +|| attrName ||+ ""
+    Just bytestringValue -> pure $ decodeUtf8 bytestringValue
 
 isXmlDocument :: Parser ()
 isXmlDocument = do
-    attr "xmlns" >>= \case
-        "urn:iso:std:iso:20022:tech:xsd:pain.001.001.03" -> pure ()
-        value -> fail $ fmt "Unknown xmlns value: " +| value |+ ""
+  attr "xmlns" >>= \case
+    "urn:iso:std:iso:20022:tech:xsd:pain.001.001.03" -> pure ()
+    value -> fail $ fmt "Unknown xmlns value: " +| value |+ ""
 
 satisfyContent :: ([Content] -> Bool) -> Parser ()
 satisfyContent predicate = Parser $ \cts ->
-    if predicate cts
-        then (Right (), cts)
-        else (Left "Content doesn't satisfy predicate", cts)
+  if predicate cts
+    then (Right (), cts)
+    else (Left "Content doesn't satisfy predicate", cts)
 
 nodeP :: Parser Node
 nodeP = do
-    [Element n] <- everythingP
-    pure n
+  [Element n] <- contentsP
+  pure n
